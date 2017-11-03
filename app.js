d3466d7898f9dc949c006ccb6ccfa3c70e11b7a9
Jonas Laursen <jona8690@edu.eal.dk>
2017-11-03 11:21:17 +0100

    EX2

--- app.js
+++ app.js
@@ -57,5 +57,37 @@ btnSingUp.addEventListener('click', e =>{
             }
     });
     
+    //get elements
+    const preObject = document.getElementById('object');
+    const ulList = document.getElementById('list');
+    
+    //create references
+    const dbRefObject= firebase.database().ref().child('object');
+    const dbRefList = dbRefObject.child('Hobbies');
+    
+    //sync object changes
+    dbRefObject.on('value', snap => {
+        preObject.innerText = JSON.stringify(snap.val(), null,  3);
+    });
+    
+    //sync list changes
+    dbRefList.on('child_added', snap => {
+        
+        const li = document.createElement('li');
+        li.innerText = snap.val();
+        li.id = snap.key;
+        ulList.appendChild(li);
+    });
+    
+    dbRefList.on('child_changed', snap => {
+        
+        const liChanged = document.getElementById(snap.key);
+        liChanged.innerText = snap.val();
+    });
+    
+    dbRefList.on('child_removed', snap=> {
+        const liToRemove = document.getElementById(snap.key);
+        liToRemove.remove();
+    })
     
 }());
